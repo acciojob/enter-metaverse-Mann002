c142a2093d496ef6dfb9d762af3dff8afefec337
Mann002 <110289540+Mann002@users.noreply.github.com>
2025-08-26 14:25:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-document.getElementById('enterBtn').addEventListner('click',functio() {
+document.getElementById('enterBtn').addEventListener('click',functio() {
 	document.getElementById('status').innerText='Entered Metaverse';
 	
 });
